70cf3912e1446d39b29a4d7fd8fce9785bb4be5f
seezelop <sebastianlopez1984@gmail.com>
2025-03-11 19:23:21 -0300

    se sigue intentando chatear

--- src/main/java/com/nexo/nexoeducativo/controller/WebSocketController.java
+++ src/main/java/com/nexo/nexoeducativo/controller/WebSocketController.java
@@ -1,9 +1,10 @@
 package com.nexo.nexoeducativo.controller;
 
+import com.nexo.nexoeducativo.models.dto.request.Mensaje;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.messaging.handler.annotation.Payload;
-import com.nexo.nexoeducativo.models.dto.request.Mensaje;
 import java.security.Principal;
 import org.springframework.messaging.simp.annotation.SendToUser;
 
@@ -14,6 +15,7 @@ public class WebSocketController {
     @SendToUser("/privado") // Envía el mensaje solo al destinatario
     public Mensaje enviarMensajePrivado(@Payload Mensaje mensaje, Principal principal) {
         System.out.println("Mensaje privado enviado por: " + principal.getName());
+        System.out.println("Contenido del mensaje: " + mensaje.getContenido());
         return mensaje;
     }
 }
